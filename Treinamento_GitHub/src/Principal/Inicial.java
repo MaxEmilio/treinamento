2dd460ff01df128685f3f49b65fb3e4826ca2c45
MaxEmilio <emiliobenjoino@gmail.com>
2017-09-06 21:41:02 -0300

    Update Inicial.java

--- Treinamento_GitHub/src/Principal/Inicial.java
+++ Treinamento_GitHub/src/Principal/Inicial.java
@@ -4,10 +4,11 @@ public class Inicial {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ol� mundo!");
+		System.out.println("Olá mundo!");
 		
 		System.out.println("update");
 		
 	}
 	
+	// comentário direto do github
 }
